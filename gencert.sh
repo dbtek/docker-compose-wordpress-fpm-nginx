2fbd3e5406a3e1a61faf6d82027221a3ec2e59c1
İsmail Demirbilek <ce.demirbilek@gmail.com>
2018-03-30 17:45:06 +0300

    update volumes

--- gencert.sh
+++ gencert.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -p 443:443 -p 80:80 --name certbot --mount src=$(pwd)/certs,target=/etc/letsencrypt,type=bind --mount src=$(pwd)/certs-data,target=/webroot,type=bind certbot/certbot certonly --standalone
+docker run -it --rm -p 443:443 -p 80:80 --name certbot --mount src=$(pwd)/le/certs,target=/etc/letsencrypt,type=bind --mount src=$(pwd)/le/data,target=/webroot,type=bind certbot/certbot certonly --standalone
