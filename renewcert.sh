3ce773a68cab9fda664521fa40444c0bd01427ad
İsmail Demirbilek <ce.demirbilek@gmail.com>
2018-04-03 13:01:17 +0300

    renew script added

---
+++ renewcert.sh
@@ -0,0 +1,1 @@
+docker run -it --rm -p 443:443 -p 80:80 --name certbot --mount src=$(pwd)/le/certs,target=/etc/letsencrypt,type=bind --mount src=$(pwd)/le/data,target=/webroot,type=bind certbot/certbot renew  --dry-run
