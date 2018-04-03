e83437cea0f59ff12f16712163c35026ee814f52
İsmail Demirbilek <ce.demirbilek@gmail.com>
2018-04-03 13:57:54 +0300

    update renew script add support for nginx reload

--- renewcert.sh
+++ renewcert.sh
@@ -1,1 +1,3 @@
-docker run -it --rm -p 443:443 -p 80:80 --name certbot --mount src=$(pwd)/le/certs,target=/etc/letsencrypt,type=bind --mount src=$(pwd)/le/data,target=/webroot,type=bind certbot/certbot renew  --dry-run
+cd "$(dirname "$0")"
+docker run -it --rm --name certbot --mount src=$(pwd)/le/certs,target=/etc/letsencrypt,type=bind --mount src=$(pwd)/le/data,target=/webroot,type=bind certbot/certbot renew --webroot --webroot-path="/webroot"
+docker-compose exec nginx nginx -s reload
